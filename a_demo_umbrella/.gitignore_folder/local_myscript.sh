a54efbb0ae8deaaf09e4db447ac8fdb43e1ca05b
Yannick Delandmeter <yannick.delandmeter@student.ucll.be>
2020-03-04 09:51:24 +0100

    Create local_myscript.sh

---
+++ a_demo_umbrella/.gitignore_folder/local_myscript.sh
@@ -0,0 +1,13 @@
+#### CONTENTS
+export DB_HOST=remotemysql.com
+export DB_USER=OaUZEnEadD
+export DB_NAME=OaUZEnEadD
+export DB_PASSWORD=aJM2nkknL0
+export DB_POOL_SIZE=1
+
+export TEST_DB_HOST=remotemysql.com
+export TEST_DB_USER=37u1GDGfOC
+export TEST_DB_NAME=37u1GDGfOC
+export TEST_DB_PASSWORD=udRMImB5XV
+export TEST_DB_POOL_SIZE=1
+### EOF
